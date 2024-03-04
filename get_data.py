d977e2b454bd2ecb945141fd30a261f3a2ad47fb
Michael Van de Ven <michaelvandeven@MBP-van-Michael.router>
2024-03-04 21:12:42 +0100

    Added dynamic value adding

--- get_data.py
+++ get_data.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 import time
 import urllib.request
-from influxdb_client import InfluxDBClient, Point
+from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
@@ -19,6 +19,17 @@ def call_url(url):
         write_to_influxdb(temperature)
         print(f"Temperature: {temperature}")
 
+def flatten_dict(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
 def write_to_influxdb(data):
     current_time = datetime.utcnow().isoformat()
 
@@ -26,7 +37,22 @@ def write_to_influxdb(data):
         .tag("host", "server01") \
         .tag("region", "eu") \
         .field("value", data) \
-        .field("time", current_time)
+        .time(datetime.utcnow(), WritePrecision.NS)
+
+    # Add a new field to the point
+    p.field("pressure", 1013.25)
+
+    nested_dict = {
+        "location": {
+            "lat": 51.0,
+            "lon": 45.0
+        }
+    }
+
+    flat_dict = flatten_dict(nested_dict)
+
+    for k, v in flat_dict.items():
+        p.field(k, v)
     
     write_api.write(bucket="home", record=p)
 
